ee9e4e5c16b6b0a815aa555e5eea003e0aca0bd7
Silvano <silvano.norberti@btinkeeng.com>
2024-05-14 15:19:15 +0200

    🚀 feat:  styled about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,8 +4,35 @@ import styles from "./about.module.css";
 const AboutPage = () => {
   return (
     <div>
-      <div className={styles.imageContainer}>
-        <Image src="/about.png" alt="about" fill />
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h2 className={styles.subTitle}>Grande testo</h2>
+          <h1 className={styles.title}>Altro testo</h1>
+          <p className={styles.desc}>description</p>
+          <div className={styles.boxes}>
+            <div className={styles.box}>
+              <h1>10 k+</h1>
+              <p>Year of experience</p>
+            </div>
+            <div className={styles.box}>
+              <h1>10 k+</h1>
+              <p>Year of experience</p>
+            </div>
+            <div className={styles.box}>
+              <h1>10 k+</h1>
+              <p>Year of experience</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+            src="/about.png"
+            alt="About image"
+            fill
+            className={styles.img}
+          />
+        </div>
       </div>
     </div>
   );
